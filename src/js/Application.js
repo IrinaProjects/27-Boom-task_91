9e9ca1a75a18b61344eabd3b7c6e0b47e01695de
Irina Spasova <irina.spasova.81@gmail.com>
2023-01-25 11:06:01 +0200

    Update Application.js

--- src/js/Application.js
+++ src/js/Application.js
@@ -2,24 +2,58 @@ import EventEmitter from "eventemitter3";
 import Beat from "./Beat";
 
 export default class Application extends EventEmitter {
-  static get events() {
-    return {
-      READY: "ready",
-    };
-  }
+    static get events() {
+        return {
+            READY: "ready",
+        };
+    }
 
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
-    let count = 0;
+        const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
+        let count = 0;
 
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = "Ah";
+        this._beat = new Beat();
+        lyrics.forEach((lyric) => {
+          this._create(lyric);
+        });
+        this._beat.on(Beat.events.BIT, () => {
+          lyrics.forEach((lyric) => {
+            this._create(lyric);
+          });
+        });
 
-    document.querySelector(".main").appendChild(message);
+        this._beat.on(Beat.events.BIT, () => {
+          this._create(lyrics[count]);
+          count++;
+          if (count === lyrics.length) {
+            count = 0;
+          }
+        });
 
-    this.emit(Application.events.READY);
-  }
+        this._beat.ontimeout = () => {
+          this._create(lyrics[count]);
+          count++;
+          if (count === lyrics.length) {
+            count = 0;
+          }
+        };
+        this._beat.addEventListener(Beat.events.BIT, () => {
+            this._create(lyrics[count]);
+            count++;
+            if (count === lyrics.length) {
+                count = 0;
+            }
+        });
+
+        this.emit(Application.events.READY);
+    }
+    _create(lyric) {
+        const message = document.createElement("div");
+        message.classList.add("message");
+        message.innerText = lyric;
+
+        document.querySelector(".main").appendChild(message);
+    }
 }
